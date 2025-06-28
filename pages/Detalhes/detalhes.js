2fa2d5d2b9d408e524ad474562f590896d0a31c2
braiatenebras <bryan.fagundes@escola.pr.gov.br>
2025-06-27 21:50:44 -0300

    arrumadno a busca na pagina de detalhes

--- pages/Detalhes/detalhes.js
+++ pages/Detalhes/detalhes.js
@@ -213,7 +213,7 @@ document.addEventListener('DOMContentLoaded', function () {
             document.querySelectorAll('.sugestao-item').forEach(item => {
                 item.addEventListener('click', function () {
                     const id = this.getAttribute('data-id');
-                    window.location.href = `pages/Detalhes/detalhes.html?id=${id}`;
+                    window.location.href = `detalhes.html?id=${id}`;
                 });
             });
         } else {
